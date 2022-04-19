953a784576e9809ae1e0532c1a52879758829709
shaundee <shawndeewilson@gmail.com>
2022-04-19 20:31:24 +0100

    scrollbar-Hidden

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -46,8 +46,9 @@ const Modal = () => {
 
   return (
     <MuiModal 
-    className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md hover:bg-[#181818]" 
-    open={showModal} onClose={handleClose}>
+    className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide" 
+    open={showModal} 
+    onClose={handleClose}>
         <>
         <button 
         onClick={handleClose}
